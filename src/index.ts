9fb936e899ac74776dd99b73e82d3be47103183a
BuildTools <unconfigured@null.spigotmc.org>
2021-03-19 20:51:00 -0500

    Initial commit with no-href-assignment rule

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import noHrefAssignment from "./rules/no-href-assignment";
+
+export = {
+  rules: {
+    "no-href-assignment": noHrefAssignment
+  },
+};
---
+++ src/rules/no-href-assignment.ts
@@ -0,0 +1,68 @@
+import { ESLintUtils, TSESTree } from "@typescript-eslint/experimental-utils";
+import { TSESTreeToTSNode } from "@typescript-eslint/typescript-estree";
+
+type MessageIds = "avoidHref";
+type Options = [];
+
+export default ESLintUtils.RuleCreator((name) => "google.com")<
+  Options,
+  MessageIds
+>({
+  name: "no-href-assignment",
+  meta: {
+    type: "suggestion",
+    fixable: "code",
+    docs: {
+      category: "Best Practices",
+      recommended: false,
+      description: "Prefer using location.assigned to make testing easier",
+    },
+    messages: {
+      avoidHref:
+        "Prefer using location.assigned instead of href direct assignments",
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+  create: function(context) {
+    return {
+      "AssignmentExpression[operator='='][left.type='MemberExpression'][left.property.name='href']": (
+        assignExp: TSESTree.AssignmentExpression
+      ) => {
+        const leftExp = assignExp.left as TSESTree.MemberExpression;
+        const parserServices = ESLintUtils.getParserServices(context);
+        let tsNode:
+          | TSESTreeToTSNode<TSESTree.Identifier>
+          | TSESTreeToTSNode<TSESTree.Expression>
+          | undefined;
+        if (leftExp.object.type === "MemberExpression") {
+          tsNode = parserServices.esTreeNodeToTSNodeMap.get(
+            (leftExp.object as TSESTree.MemberExpression).property
+          );
+        } else if (leftExp.object.type === "Identifier") {
+          tsNode = parserServices.esTreeNodeToTSNodeMap.get(leftExp.object);
+        }
+
+        if (tsNode) {
+          const typeChecker = parserServices.program.getTypeChecker();
+          const type = typeChecker.getTypeAtLocation(tsNode);
+          if (type.getSymbol().getEscapedName() === "Location") {
+            context.report({
+              node: assignExp,
+              messageId: "avoidHref",
+              fix: function(fixer) {
+                return [
+                  fixer.replaceTextRange(
+                    [leftExp.property.range[0], assignExp.right.range[0]],
+                    "assign("
+                  ),
+                  fixer.insertTextAfter(assignExp.right, ")"),
+                ];
+              },
+            });
+          }
+        }
+      },
+    };
+  },
+});
