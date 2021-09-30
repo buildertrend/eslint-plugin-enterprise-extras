97515816f7df72dd8738d0cdb3ac8b3eb45be309
Cameron Hessler <cameron.hessler@buildertrend.com>
2021-09-30 17:42:32 -0500

    Add unregister-events rule

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import noHrefAssignment from "./rules/no-href-assignment";
 import noUnhandledScheduling from "./rules/no-unhandled-scheduling";
 import privateComponentMethods from "./rules/private-component-methods";
+import unregisterEvents from "./rules/unregister-events";
 
 export = {
   rules: {
     "no-href-assignment": noHrefAssignment,
     "private-component-methods": privateComponentMethods,
     "no-unhandled-scheduling": noUnhandledScheduling,
+    "unregister-events": unregisterEvents,
   },
   configs: {
     recommended: {
@@ -15,6 +17,7 @@ export = {
         "enterprise-extras/no-href-assignment": "error",
         "enterprise-extras/private-component-methods": "error",
         "enterprise-extras/no-unhandled-scheduling": "warn",
+        "enterprise-extras/unregister-events": "error",
       },
     },
     all: {
@@ -23,6 +26,7 @@ export = {
         "enterprise-extras/no-href-assignment": "error",
         "enterprise-extras/private-component-methods": "error",
         "enterprise-extras/no-unhandled-scheduling": "error",
+        "enterprise-extras/unregister-events": "error",
       },
     },
   },
---
+++ src/rules/unregister-events.ts
@@ -0,0 +1,146 @@
+import { ESLintUtils, TSESTree } from "@typescript-eslint/experimental-utils";
+
+type MessageIds = "unregisterEventsInClass" | "unregisterEventsInHook";
+type Options = [];
+
+interface ISubscriptionStack {
+  callExpression: TSESTree.Node;
+  eventHandler: TSESTree.Node;
+  eventName: string;
+}
+
+export default ESLintUtils.RuleCreator(
+  name =>
+    `https://github.com/buildertrend/eslint-plugin-enterprise-extras/blob/main/docs/${name}.md`
+)<Options, MessageIds>({
+  name: "unregister-events",
+  meta: {
+    type: "suggestion",
+    docs: {
+      category: "Best Practices",
+      recommended: "warn",
+      description:
+        "When using Javascript scheduling (`setTimeout` or `setInterval`), it is recommended to support cancelling of the task, especially within React components."
+    },
+    messages: {
+      unregisterEventsInClass:
+        "All add event listener calls must have a corresponding unregister event call in componentWillUnmount",
+      unregisterEventsInHook:
+        "All add event listener calls must have a corresponding unregister event call in a useEffect cleanup function"
+    },
+    schema: []
+  },
+  defaultOptions: [],
+  create: function(context) {
+    let stack: ISubscriptionStack[] = [];
+
+    const isSameSubscription = (
+      sub1: ISubscriptionStack,
+      sub2: ISubscriptionStack
+    ) => {
+      if (sub1.eventName === sub2.eventName) {
+        const handler1Tokens = context
+          .getSourceCode()
+          .getTokens(sub1.eventHandler);
+        const handler2Tokens = context
+          .getSourceCode()
+          .getTokens(sub2.eventHandler);
+        return (
+          handler1Tokens.length === handler2Tokens.length &&
+          handler1Tokens.every((t1, ind) => {
+            return t1.value === handler2Tokens[ind].value;
+          })
+        );
+      }
+
+      return false;
+    };
+
+    const clearStack = () => {
+      stack = [];
+    };
+
+    const reportStack = (componentType: "hook" | "classComponent") => {
+      stack.forEach(error => {
+        context.report({
+          node: error.callExpression,
+          messageId:
+            componentType === "classComponent"
+              ? "unregisterEventsInClass"
+              : "unregisterEventsInHook"
+        });
+      });
+
+      clearStack();
+    };
+
+    const pushStack = (callExpression: TSESTree.CallExpression) => {
+      // If the callExpression fails these checks, chances are you have compiler errors anyways, so we can ignore adding to the stack
+      if (
+        callExpression.arguments.length >= 2 &&
+        callExpression.arguments.length <= 3
+      ) {
+        let eventType = callExpression.arguments[0];
+        let handler = callExpression.arguments[1];
+
+        if (
+          eventType.type === "Literal" &&
+          typeof eventType.value === "string"
+        ) {
+          const subscription = {
+            eventName: eventType.value,
+            eventHandler: handler,
+            callExpression: callExpression
+          };
+          stack.push(subscription);
+        }
+      }
+    };
+
+    const popStack = (callExpression: TSESTree.CallExpression) => {
+      // If the callExpression fails these checks, chances are you have compiler errors anyways, so we can ignore adding to the stack
+      if (
+        callExpression.arguments.length >= 2 &&
+        callExpression.arguments.length <= 3
+      ) {
+        let eventType = callExpression.arguments[0];
+        let handler = callExpression.arguments[1];
+
+        if (
+          eventType.type === "Literal" &&
+          typeof eventType.value === "string"
+        ) {
+          const eventName = eventType.value;
+          const subscription: ISubscriptionStack = {
+            callExpression: callExpression,
+            eventName: eventName,
+            eventHandler: handler
+          };
+
+          stack = stack.filter(existingSubscription => {
+            return !isSameSubscription(subscription, existingSubscription);
+          });
+        }
+      }
+    };
+
+    return {
+      Program: clearStack,
+      "Program:exit": clearStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] CallExpression[callee.name='addEventListener']": pushStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] CallExpression[callee.object.name='window'][callee.property.name='addEventListener']": pushStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] MethodDefinition[key.name='componentWillUnmount'] CallExpression[callee.object.name='window'][callee.property.name='removeEventListener']": popStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] MethodDefinition[key.name='componentWillUnmount'] CallExpression[callee.name='removeEventListener']": popStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] ClassProperty[key.name='componentWillUnmount'] CallExpression[callee.object.name='window'][callee.property.name='removeEventListener']": popStack,
+      "ClassDeclaration[superClass.property.name=/Component|PureComponent/] ClassProperty[key.name='componentWillUnmount'] CallExpression[callee.name='removeEventListener']": popStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.name='addEventListener']": pushStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.object.name='window'][callee.property.name='addEventListener']": pushStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.name='useEffect'] > ArrowFunctionExpression > BlockStatement > ReturnStatement CallExpression[callee.name='removeEventListener']": popStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.name='useEffect'] > ArrowFunctionExpression > BlockStatement > ReturnStatement CallExpression[callee.object.name='window'][callee.property.name='removeEventListener']": popStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.object.name='React'][callee.property.name='useEffect'] > ArrowFunctionExpression > BlockStatement > ReturnStatement CallExpression[callee.name='removeEventListener']": popStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/] CallExpression[callee.object.name='React'][callee.property.name='useEffect'] > ArrowFunctionExpression > BlockStatement > ReturnStatement CallExpression[callee.object.name='window'][callee.property.name='removeEventListener']": popStack,
+      "VariableDeclarator[id.name=/^[A-Z].+/]:exit": () => reportStack("hook"),
+      "ClassDeclaration:exit": () => reportStack("classComponent")
+    };
+  }
+});
