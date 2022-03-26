7db06510ec49f721c7a06f388d23f27b1208d83a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-26 14:48:21 -0500

    Initial commit of no-unstable-dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import noHrefAssignment from "./rules/no-href-assignment";
 import noUnhandledScheduling from "./rules/no-unhandled-scheduling";
 import privateComponentMethods from "./rules/private-component-methods";
 import unregisterEvents from "./rules/unregister-events";
+import preventUnstableDependencies from "./rules/prevent-unstable-dependencies";
 
 export = {
   rules: {
@@ -9,6 +10,7 @@ export = {
     "private-component-methods": privateComponentMethods,
     "no-unhandled-scheduling": noUnhandledScheduling,
     "unregister-events": unregisterEvents,
+    "prevent-unstable-dependencies": preventUnstableDependencies,
   },
   configs: {
     recommended: {
@@ -18,6 +20,7 @@ export = {
         "enterprise-extras/private-component-methods": "error",
         "enterprise-extras/no-unhandled-scheduling": "warn",
         "enterprise-extras/unregister-events": "error",
+        "enterprise-extras/prevent-unstable-dependencies": "warn",
       },
     },
     all: {
@@ -27,6 +30,7 @@ export = {
         "enterprise-extras/private-component-methods": "error",
         "enterprise-extras/no-unhandled-scheduling": "error",
         "enterprise-extras/unregister-events": "error",
+        "enterprise-extras/prevent-unstable-dependencies": "error",
       },
     },
   },
--- src/utils/type-guards.ts
+++ src/utils/type-guards.ts
@@ -24,14 +24,71 @@ export const isMethod = (
   );
 };
 
+export const isFunctionDeclaration = (
+  node: TSESTree.Node
+): node is TSESTree.ArrowFunctionExpression | TSESTree.FunctionDeclaration => {
+  return (
+    node.type === "ArrowFunctionExpression" ||
+    node.type === "FunctionDeclaration"
+  );
+};
+
 export const isIdentifier = (
   node: TSESTree.Expression | TSESTree.PrivateIdentifier
 ): node is TSESTree.Identifier => {
   return node.type === "Identifier";
 };
 
+export const isMemberExpression = (
+  node: TSESTree.Expression
+): node is TSESTree.MemberExpression => {
+  return node.type === "MemberExpression";
+};
+
 export const isAsExpression = (
   node: TSESTree.Expression
 ): node is TSESTree.TSAsExpression => {
   return node.type === "TSAsExpression";
+};
+
+const setOfUnstableAssignmentTypes = new Set([
+  "NewExpression",
+  "ObjectExpression",
+  "ArrayExpression",
+  "ArrowFunctionExpression",
+  "FunctionExpression",
+]);
+type UnstableExpression =
+  | TSESTree.NewExpression
+  | TSESTree.ObjectExpression
+  | TSESTree.ArrayExpression
+  | TSESTree.ArrowFunctionExpression
+  | TSESTree.FunctionExpression;
+export const isUnstableExpression = (
+  node: TSESTree.Node
+): node is UnstableExpression => {
+  return setOfUnstableAssignmentTypes.has(node.type);
+};
+
+export const isAssignmentPattern = (
+  node: TSESTree.Node
+): node is TSESTree.AssignmentPattern => {
+  return node.type === "AssignmentPattern";
+};
+
+export const isVariableDeclarator = (
+  node: TSESTree.Node
+): node is TSESTree.VariableDeclarator => {
+  return node.type === "VariableDeclarator";
+};
+
+export const isUnstableAssignment = (node: TSESTree.Node) => {
+  let expression: null | TSESTree.Expression = null;
+  if (isAssignmentPattern(node)) {
+    expression = node.right;
+  } else if (isVariableDeclarator(node)) {
+    expression = node.init;
+  }
+
+  return expression && isUnstableExpression(expression);
 };
